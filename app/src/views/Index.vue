6fdef2f4fcd22065874ed7291733b4dbbf232484
Satoru Nakamura <na.kamura.1263@gmail.com>
2020-05-01 09:12:25 +0900

    minour update

--- app/src/views/Index.vue
+++ app/src/views/Index.vue
@@ -6,9 +6,11 @@
 
         <v-spacer></v-spacer>
 
+        <!-- 
         <v-toolbar-items>
           <v-btn text :to="{name : 'test', query : {'u': $route.query.u}}">新しいUIを試す</v-btn>
         </v-toolbar-items>
+        -->
         <v-btn icon @click.stop="dialog_settings = true">
           <v-icon>mdi-settings</v-icon>
         </v-btn>
